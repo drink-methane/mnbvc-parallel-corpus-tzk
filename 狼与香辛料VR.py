9f4a270f711df268513d651b3b96b54659a1244a
Ken <673508050@qq.com>
2025-09-08 14:48:39 +0800

    SpiceAndWolfVR2

---
+++ 狼与香辛料VR.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import tools as tos
+import subprocess
+import json
+from pathlib import Path
+
+def aliandmove(source, target):
+    for root, dirs, files in os.walk(source):
+        for file in files:
+            file_path = os.path.join(root, file)#原文件的位置
+            if file_path.endswith('.json'):
+                with open(file_path, 'r', encoding = "UTF-8") as f:# 读取文件内容
+                    content = json.load(f)
+                text = content["textList"]
+                end, jad, zhsd, zhtd = {}, {}, {}, {}
+                for i in text:
+                    end[i["stringID"]] = i["stringEN"]
+                    jad[i["stringID"]] = i["stringJP"]
+                    zhsd[i["stringID"]] = i["stringSC"]
+                    zhtd[i["stringID"]] = i["stringTC"]
+                # enf, jaf, zhsf, zhtf = r""# 各自的目标路径
+                enf = os.path.join(target, "en", tos.ha(file_path) + ".json")
+                jaf = os.path.join(target, "ja", tos.ha(file_path) + ".json")
+                zhsf = os.path.join(target, "zhs", tos.ha(file_path) + ".json")
+                zhtf = os.path.join(target, "zht", tos.ha(file_path) + ".json")
+                if not os.path.exists(os.path.join(target, "en")):
+                    os.makedirs(os.path.join(target, "en"))
+                if not os.path.exists(os.path.join(target, "ja")):
+                    os.makedirs(os.path.join(target, "ja"))
+                if not os.path.exists(os.path.join(target, "zhs")):
+                    os.makedirs(os.path.join(target, "zhs"))
+                if not os.path.exists(os.path.join(target, "zht")):
+                    os.makedirs(os.path.join(target, "zht"))
+                with open(enf, 'w', encoding='utf-8') as f:# 将数据写入新的.json文件
+                    json.dump(end, f, ensure_ascii=False, indent=4)
+                with open(jaf, 'w', encoding='utf-8') as f:# 将数据写入新的.json文件
+                    json.dump(jad, f, ensure_ascii=False, indent=4)
+                with open(zhsf, 'w', encoding='utf-8') as f:# 将数据写入新的.json文件
+                    json.dump(zhsd, f, ensure_ascii=False, indent=4)
+                with open(zhtf, 'w', encoding='utf-8') as f:# 将数据写入新的.json文件
+                    json.dump(zhtd, f, ensure_ascii=False, indent=4)
+    return 0
+
+source = r"D:\MNBVC\SpiceAndWolfVR2"
+target = r"D:\sandbox\data"
+aliandmove(source, target)
